eb24d952e00a664b891635c98ce8c337d4299156
jonathan <jonathan.123kutumbakana@gmail.com>
2024-01-15 22:01:59 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const namesArray = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(namesArray, event) {
+  const thankYouMessages = [];
+  for (let i = 0; i < namesArray.length; i++) {
+    thankYouMessages.push(
+      `Thank you, ${namesArray[i]}, for the wonderful surprise gift!`
+    );
+  }
+  return thankYouMessages;
+}
+
+function countDown(number) {
+  let i = number;
+  while (i >= 0) {
+    console.log(i);
+    i--;
+  }
+  return number;
+}
+countDown(10);
